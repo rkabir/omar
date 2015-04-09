6243c84e3caed64f619b63b26b246f9bf1559df7
Ryan Kabir <rkabir@alum.mit.edu>
2015-04-08 22:46:17 -0400

    fix keys

--- kill_screens.py
+++ kill_screens.py
@@ -4,8 +4,8 @@ keys = ["twentysix", "twentyseven", "twentyeight", "twentynine", "thirty",
     "twentyone", "twentytwo", "twentythree", "twentyfour", "twentyfive",
     "sixteen", "seventeen", "eighteen", "nineteen", "twenty",
     "eleven", "twelve", "thirteen", "fourteen", "fifteen",
-    "six", "seven", "eight", "nine", "ten",
-    "one", "two", "three", "four", "five"]
+    "ohsix", "ohseven", "oheight", "ohnine", "ten",
+    "ohone", "ohtwo", "ohthree", "ohfour", "ohfive"]
 
 for key in keys:
     os.popen("screen -r %s -p 0 -X stuff \"exit\n\"" % (key))
